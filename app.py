e318b1f52015bdc6ece0d05eb679ea8383b4809a
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-06-09 17:56:12 -0300

    feat:implementing dynamic information in payment.html

--- app.py
+++ app.py
@@ -59,9 +59,25 @@ def get_image(file_name):
 def pix_confirmation(): # webhook pois sera requisitado pra informar 
     return jsonify({"message":"The payment has been confirmed"}), 200
 
+
+'''
+---------------------------------------------------------------------
+        RENDERIZANDO UMA PÁGINA HTML
+---------------------------------------------------------------------
+'''
 @app.route("/payments/pix/<int:payment_id>", methods=['GET'])
 def payment_pix_page(payment_id):
-    return render_template('payment.html')
+    payment = Payment.query.get(payment_id)
+
+
+
+    
+    return render_template('payment.html',
+                           payment_id = payment.id, 
+                           value=payment.value,
+                           host="http://127.0.0.1:5000", 
+                           qr_code=payment.qr_code)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
